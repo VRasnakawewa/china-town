e1e18e7cb9a0c434beaec0b893153c28f5ee546b
Sujanan Bhathiya <sujananbhathiya@gmail.com>
2018-07-02 18:31:48 +0530

    Add mysql dependency

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const {app, BrowserWindow} = require('electron')
-  
-  function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
-  
-    // and load the index.html of the app.
-    win.loadFile('index.html')
-  }
-  
-  app.on('ready', createWindow)
+const { app, BrowserWindow } = require('electron')
+
+/**
+ * Create browser window and load a html page
+ */
+function createWindow () {
+    win = new BrowserWindow({width: 800, height: 600});
+    win.loadFile('index.html');
+}
+
+app.on('ready', createWindow)
